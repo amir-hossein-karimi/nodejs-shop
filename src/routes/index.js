3bbd1989e829fa89a2cb0b3a582dcd72c04d404a
amir hossein <a.hosseinkarimi82@gmail.com>
2022-12-14 12:30:48 -0800

    🔥 remove index route and its swagger

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,24 +2,6 @@ const Router = require("express").Router();
 const { authRoutes } = require("./auth");
 const { productsRoutes } = require("./products");
 const { usersRoutes } = require("./user");
-/**
- * @swagger
- * tags:
- *  name: index
- */
-/**
- * @swagger
- * /:
- *  get:
- *    summary: index
- *    tags: [index]
- *    responses:
- *      200:
- *        description: success
- */
-Router.get("/", (req, res, next) => {
-  res.send("index page");
-});
 
 Router.use("/auth", authRoutes);
 Router.use("/users", usersRoutes);
